d7ab3f211af77b2f9b446c615205e260201d8e82
Aivanii <aivanellok@gmail.com>
2025-03-11 10:17:15 +0700

    added icons into card skills

--- src/entities/MySkills/MySkillsComponent.tsx
+++ src/entities/MySkills/MySkillsComponent.tsx
@@ -8,24 +8,52 @@ const MySkills = () => {
         <div />
         <div />
         <div className="card">
+          <img
+            src="https://img.icons8.com/?size=100&id=asWSSTBrDlTW&format=png&color=000000"
+            alt="react logo"
+          />
           <h3>React</h3>
         </div>
         <div className="card">
+          <img
+            src="https://img.icons8.com/?size=100&id=Xf1sHBmY73hA&format=png&color=000000"
+            alt="TypeScript logo"
+          />
           <h3>TypeScript</h3>
         </div>
         <div className="card">
+          <img
+            src="https://sass-lang.com/assets/img/logos/logo.svg"
+            alt="SCSS logo"
+          />
           <h3>SCSS</h3>
         </div>
         <div className="card">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSDKn3vA2YUbXzN0ZC3gALWJ08gJN-Drl15w&s"
+            alt="Tailwind icon"
+          />
           <h3>Tailwind</h3>
         </div>
         <div className="card">
+          <img
+            src="https://img.icons8.com/?size=100&id=dJjTWMogzFzg&format=png&color=000000"
+            alt="Vite logo"
+          />
           <h3>Vite</h3>
         </div>
         <div className="card">
+          <img
+            src="https://img.icons8.com/?size=100&id=QjbHx7WUskg1&format=png&color=000000"
+            alt="Webpack logo"
+          />
           <h3>WebPack</h3>
         </div>
         <div className="card">
+          <img
+            src="https://img.icons8.com/?size=100&id=PndQWK6M1Hjo&format=png&color=000000"
+            alt="Bootstrap logo"
+          />
           <h3>Bootstrap</h3>
         </div>
         <div className="card">
