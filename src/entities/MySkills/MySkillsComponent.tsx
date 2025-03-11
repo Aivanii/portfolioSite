bb15aee4d152b8e4ca18684c1e95d3dcc65683b6
Aivanii <aivanellok@gmail.com>
2025-03-11 10:00:46 +0700

    added fade in anim for cards

--- src/entities/MySkills/MySkillsComponent.tsx
+++ src/entities/MySkills/MySkillsComponent.tsx
@@ -1,11 +1,39 @@
-import { useEffect } from "react";
-
 const MySkills = () => {
-  useEffect(() => {
-    
-  }, []);
   return (
-    <div>something</div>
+    <div className="mySkills-container">
+      <div className="wrap">
+        <h2>My skills</h2>
+        <div />
+        <div />
+        <div className="card">
+          <h3>React</h3>
+        </div>
+        <div className="card">
+          <h3>TypeScript</h3>
+        </div>
+        <div className="card">
+          <h3>SCSS</h3>
+        </div>
+        <div className="card">
+          <h3>Tailwind</h3>
+        </div>
+        <div className="card">
+          <h3>Vite</h3>
+        </div>
+        <div className="card">
+          <h3>WebPack</h3>
+        </div>
+        <div className="card">
+          <h3>Bootstrap</h3>
+        </div>
+        <div className="card">
+          <h3>Learning..</h3>
+        </div>
+        <div className="card">
+          <h3>Learning</h3>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/features/AnimsLogic/SkillCards/FadeInAndOutSkillCard.ts
+++ src/features/AnimsLogic/SkillCards/FadeInAndOutSkillCard.ts
@@ -1,2 +1,37 @@
-const FadeInAndOutSkillCard = () => {};
+const FadeInAndOutSkillCard = () => {
+  const cardsElems = document.querySelectorAll(".card");
+  const cardsContainer = document.querySelector(".mySkills-container");
+  const profileContainer = document.querySelector(
+    ".profile-container"
+  ) as HTMLElement;
+
+  if (!cardsElems || !cardsContainer) return;
+
+  const heightOfParentContainers =
+    cardsContainer.clientHeight + profileContainer.clientHeight;
+
+  if (window.scrollY - profileContainer.clientHeight < 0) return;
+
+  const scrolledHeightOfDocument = heightOfParentContainers - window.scrollY;
+
+  //0-100%
+  const percentageOfScrolledOfSkillCardsContainer = Number(
+    (
+      100 -
+      ((scrolledHeightOfDocument - profileContainer.clientHeight) /
+        (heightOfParentContainers - profileContainer.clientHeight) +
+        0.4) *
+        100
+    ).toFixed(2)
+  );
+  const intervalBetweenCards = 70 / cardsElems.length;
+  cardsElems.forEach((card, index) => {
+    card.style.opacity =
+      (percentageOfScrolledOfSkillCardsContainer -
+        intervalBetweenCards * index) /
+      intervalBetweenCards;
+  });
+  console.log(percentageOfScrolledOfSkillCardsContainer);
+};
+
 export default FadeInAndOutSkillCard;
