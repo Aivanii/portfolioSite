4db03093d5fada690922141391d2badb2bb0c9e9
Aivanii <aivanellok@gmail.com>
2025-03-12 10:53:16 +0700

    added random img into projects component

--- src/entities/MyProjects/MyProjectsComponent.tsx
+++ src/entities/MyProjects/MyProjectsComponent.tsx
@@ -1,7 +1,52 @@
 const MyProjects = () => {
   return (
     <div className="myProjects-container">
-      <div className="wrap">myprojects</div>
+      <div className="wrap">
+        <div>
+          <a href="https://github.com/">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
+              alt="my projects"
+            />
+          </a>
+        </div>
+
+        <div>
+          <a href="https://github.com/">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
+              alt="my projects"
+            />
+          </a>
+        </div>
+
+        <div>
+          <a href="https://github.com/">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
+              alt="my projects"
+            />
+          </a>
+        </div>
+
+        <div>
+          <a href="https://github.com/">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
+              alt="my projects"
+            />
+          </a>
+        </div>
+
+        <div>
+          <a href="https://github.com/">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
+              alt="my projects"
+            />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
