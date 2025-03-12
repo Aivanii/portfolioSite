3231ee9cf19cb2a33480305ce4fea642c5f89b4b
Aivanii <aivanellok@gmail.com>
2025-03-12 14:11:00 +0700

    added one line of projects

--- src/entities/MyProjects/MyProjectsComponent.tsx
+++ src/entities/MyProjects/MyProjectsComponent.tsx
@@ -2,65 +2,40 @@ const MyProjects = () => {
   return (
     <div className="myProjects-container">
       <div className="wrap">
-        <div data-rotate="0" className="rotate">
-          <a href="https://github.com/">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
-              alt="my projects"
-            />
-          </a>
-        </div>
-
-        <div data-rotate="60" className="rotate">
-          <a href="https://github.com/">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
-              alt="my projects"
-            />
-          </a>
-        </div>
-
-        <div data-rotate="120" className="rotate">
-          <a href="https://github.com/">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
-              alt="my projects"
-            />
-          </a>
-        </div>
-
-        <div data-rotate="180" className="rotate">
-          <a href="https://github.com/">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
-              alt="my projects"
-            />
-          </a>
-        </div>
-
-        <div data-rotate="240" className="rotate">
-          <a href="https://github.com/">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
-              alt="my projects"
-            />
-          </a>
-        </div>
-        <div data-rotate="300" className="rotate">
-          <a href="https://github.com/">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
-              alt="my projects"
-            />
-          </a>
-        </div>
-        <div data-rotate="360" className="rotate">
-          <a href="https://github.com/">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
-              alt="my projects"
-            />
-          </a>
+        {/* first */}
+        <div className="lineOfProjects">
+          <div className="rotate"
+            style={{ transform: "rotate(-35deg)", marginRight: "4em", paddingTop: "10em" }}>
+            <a href="https://github.com/">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
+                alt="my projects"
+              />
+            </a>
+          </div>
+          <div className="rotate">
+            <a href="https://github.com/">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
+                alt="my projects"
+              />
+            </a>
+          </div>
+          <div
+            className="rotate"
+            style={{
+              transform: "rotate(35deg)",
+              marginLeft: "4em",
+              paddingTop: "10em",
+            }}
+          >
+            <a href="https://github.com/">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
+                alt="my projects"
+              />
+            </a>
+          </div>
         </div>
       </div>
     </div>
