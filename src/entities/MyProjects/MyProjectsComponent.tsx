5e3f6b511b5b13478381e370df246341ac70381c
Aivanii <aivanellok@gmail.com>
2025-03-12 14:16:48 +0700

    deleted my projects

--- src/entities/MyProjects/MyProjectsComponent.tsx
+++ src/entities/MyProjects/MyProjectsComponent.tsx
@@ -2,41 +2,7 @@ const MyProjects = () => {
   return (
     <div className="myProjects-container">
       <div className="wrap">
-        {/* first */}
-        <div className="lineOfProjects">
-          <div className="rotate"
-            style={{ transform: "rotate(-35deg)", marginRight: "4em", paddingTop: "10em" }}>
-            <a href="https://github.com/">
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
-                alt="my projects"
-              />
-            </a>
-          </div>
-          <div className="rotate">
-            <a href="https://github.com/">
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
-                alt="my projects"
-              />
-            </a>
-          </div>
-          <div
-            className="rotate"
-            style={{
-              transform: "rotate(35deg)",
-              marginLeft: "4em",
-              paddingTop: "10em",
-            }}
-          >
-            <a href="https://github.com/">
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
-                alt="my projects"
-              />
-            </a>
-          </div>
-        </div>
+        
       </div>
     </div>
   );
