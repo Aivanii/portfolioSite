0a6851beaf4afcef922ef5a65f3c263870e4a357
Aivanii <aivanellok@gmail.com>
2025-03-12 11:04:17 +0700

    added broken rotate for projects

--- src/entities/MyProjects/MyProjectsComponent.tsx
+++ src/entities/MyProjects/MyProjectsComponent.tsx
@@ -2,7 +2,7 @@ const MyProjects = () => {
   return (
     <div className="myProjects-container">
       <div className="wrap">
-        <div>
+        <div data-rotate="0" className="rotate">
           <a href="https://github.com/">
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
@@ -11,7 +11,7 @@ const MyProjects = () => {
           </a>
         </div>
 
-        <div>
+        <div data-rotate="60" className="rotate">
           <a href="https://github.com/">
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
@@ -20,7 +20,7 @@ const MyProjects = () => {
           </a>
         </div>
 
-        <div>
+        <div data-rotate="120" className="rotate">
           <a href="https://github.com/">
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
@@ -29,7 +29,7 @@ const MyProjects = () => {
           </a>
         </div>
 
-        <div>
+        <div data-rotate="180" className="rotate">
           <a href="https://github.com/">
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
@@ -38,7 +38,23 @@ const MyProjects = () => {
           </a>
         </div>
 
-        <div>
+        <div data-rotate="240" className="rotate">
+          <a href="https://github.com/">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
+              alt="my projects"
+            />
+          </a>
+        </div>
+        <div data-rotate="300" className="rotate">
+          <a href="https://github.com/">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
+              alt="my projects"
+            />
+          </a>
+        </div>
+        <div data-rotate="360" className="rotate">
           <a href="https://github.com/">
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRj5cHt1ugymFusNUPSinFEWoey706jiBDJg&s"
