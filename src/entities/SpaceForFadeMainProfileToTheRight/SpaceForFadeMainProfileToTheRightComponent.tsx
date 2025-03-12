25598500fcf9fd6b925ac213fc5f77bec362d534
Aivanii <aivanellok@gmail.com>
2025-03-12 20:57:41 +0700

    added files for deploy

--- src/entities/SpaceForFadeMainProfileToTheRight/SpaceForFadeMainProfileToTheRightComponent.tsx
+++
@@ -1,9 +0,0 @@
-const SpaceForFadeMainProfileToTheRight = () => {
-  return (
-    <div className="SpaceToTheRight">
-      <div className="wrap"></div>
-    </div>
-  );
-};
-
-export default SpaceForFadeMainProfileToTheRight;
